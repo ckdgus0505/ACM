b54cdbe3acb74068ed645d8d1cd961ed178bfa2f
dongseoki <43495319+dongseoki@users.noreply.github.com>
2019-03-24 20:22:31 +0900

    homework end!!

--- dongseoki/ACM/2ntiling.cpp
+++ dongseoki/ACM/2ntiling.cpp
@@ -13,11 +13,7 @@ int main(void) {
 	ios::sync_with_stdio(false); //  printf 와 scanf 를 못사용하게 하면서 속도 향상을 시킴. 동기화를 해제한다.?
 	cin.tie(NULL); // cout 과 cin 사이에 같이 쓰는 버퍼 연결을 끊는다. 동기화를 해제한다.?
 	cin >> n;
-	memset(cache, -1, sizeof(cache));
-
 	cout << n2_tiling(n);
-	
-	p.second.first = 3;
 	return 0;
 }
 
@@ -26,12 +22,20 @@ int n2_tiling(int n) {
 		return 1;
 	if (n == 2)
 		return 2;
-
-	int & ref = cache[n];
-	if (ref != -1)
-		return ref;
-
-	ref = 2 * n2_tiling(n - 2) + n2_tiling(n-1) -1;
-
-	return ref;
+	if (n == 3)
+		return 3;
+	int sum = 0;
+	vector<int> tile;// tile(1001) 이런식으로 했더니 문제가 생겼다 왜일까..
+	tile.push_back(0);// index 0.. will not use.
+	tile.push_back(1);
+	tile.push_back(2);
+	tile.push_back(3);
+	for (vector<int>::size_type i = 4; i <= n; i++) {
+		//cout << tile[i - 2] << " " << tile[i - 3] << "\n";
+		sum = tile[i-2] * 2 + tile[i-3];
+		sum %= 10007;
+		tile.push_back(sum);
+	}
+	
+	return sum;
 }
