0ace4be5f595c0459a943cd04e3dcb286e024adc
ImSangKyun <30777239+ImSangKyun@users.noreply.github.com>
2019-03-27 18:56:36 +0900

    난 멍청했다
    출력할때
    cout << "result : " << func(W) << '\n';
    해가지고 틀리는걸 왜틀리지 하면서 1시간 쳐다보았다... 다들 이런실수하지 말길... 결과는 무조건 숫자만!

--- ImSangKyun/ACM/VisualProject/VisualProject/1005_ACMCraft.cpp
+++ ImSangKyun/ACM/VisualProject/VisualProject/1005_ACMCraft.cpp
@@ -3,16 +3,15 @@
 #include <vector>
 using namespace std;
 
-int N, K;
-vector<vector<int>> arr;
-vector<int> time;
-vector<int> cache;
+int N, K, W;
+vector<vector<int>> arr;	//노드 순서로, x->y 일때 arr[y].push_back(x) 로 역순저장
+vector<int> time;	//각 건물을 짓는데 필요한 시간
+vector<int> cache;	//메모이제이션
 
 int func(int node) {
 	int& ret = cache[node];
-	if (ret != -1)	return ret;
-	if (node == 1)	return time[node];
-	ret = -1;
+	if (ret != -1)	return ret;	
+	if (arr[node].empty())	return time[node];	//시작지점이 반드시 1이 아니므로 시작으로 간주될 법한 노드는 바로 리턴
 	for (auto iter : arr[node]) {
 		ret = max(ret, func(iter) + time[node]);
 	}
@@ -23,13 +22,13 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int T, W;
+	int T;
 	cin >> T;
 
 	for (int lo = 0; lo < T; ++lo) {
 		cin >> N >> K;
 		arr = vector<vector<int>>(N+1);
-		time = vector<int>(N + 1);
+		time = vector<int>(N + 1, 0);
 		cache = vector<int>(N + 1, -1);
 		for (int i = 1; i <= N; ++i) 
 			cin >> time[i];
@@ -40,7 +39,7 @@ int main(void) {
 		}
 		cin >> W;
 
-		cout << "result : " << func(W) << '\n';
+		cout << func(W) << '\n';
 	}
 
 }
