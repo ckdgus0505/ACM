6e0f98fd71c33a6b47c62e3cb6c9842392ac5632
ckdgus0505 <ckdgus0505@gmail.com>
2019-04-01 01:54:57 +0900

    solve 1992

---
+++ ckdgus0505/ACM/1992.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdio>
+#include<vector>
+#include<cstring>
+using namespace std;
+int N;
+vector<vector<int>> arr;
+
+int quadTree(int x, int y, int size) // x,y 는 시작 위치
+{
+	vector<vector<int>> arr0(size, vector<int>(size, 0));
+	vector<vector<int>> arr1(size, vector<int>(size, 1));
+	vector<vector<int>> subarr(size, vector<int>(size, 0));
+	for (int i = x; i < x + size; i++)
+	{
+		for (int j = y; j < y + size; j++)
+		{
+			subarr[i - x][j - y] = arr[i][j];
+		}
+	}
+	if (subarr == arr0)
+	{
+		cout << '0';
+		return 0;
+	}
+	else if (subarr == arr1)
+	{
+		cout << '1';
+		return 0;
+	}
+	arr0.clear(); // 메모리 해제
+	arr1.clear();
+	subarr.clear();
+	cout << '(';
+	quadTree(x, y, size / 2);
+	quadTree(x, y + size / 2, size / 2);
+	quadTree(x + size / 2, y, size / 2);
+	quadTree(x + size / 2, y + size / 2, size / 2);
+	cout << ')';
+}
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	arr.assign(N, vector<int>(N, 0));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			scanf("%1d", &arr[i][j]);
+		}
+	}
+	quadTree(0, 0, N);
+	return 0;
+}
