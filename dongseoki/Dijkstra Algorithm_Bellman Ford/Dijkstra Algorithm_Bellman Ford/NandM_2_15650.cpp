ca8d833acc1af6c52c7d6120abc53c8343884821
dongseoki <43495319+dongseoki@users.noreply.github.com>
2019-06-28 08:32:08 +0900

    Create NandM_2_15650.cpp

---
+++ dongseoki/Dijkstra Algorithm_Bellman Ford/Dijkstra Algorithm_Bellman Ford/NandM_2_15650.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	int N;
+	int M;
+	ios_base::sync_with_stdio(0); // https://en.cppreference.com/w/cpp/io/ios_base/sync_with_stdio 싱크를 안하면 속도가 빨라진다 단일로 사용가능하다를 알수 있음.
+	cin.tie(0); // https://codecollector.tistory.com/381 , https://su-m.tistory.com/7 , tie 안하면 입력을 먼저 받게 된다.  속도 향상은 가능
+	cin >> N;
+	cin >> M;
+
+	
+	return 0;
+}
