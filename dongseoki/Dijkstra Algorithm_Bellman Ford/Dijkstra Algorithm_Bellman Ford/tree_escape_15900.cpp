af6ff4b6ca947f90b7e0f3461241cfa13e5984a8
dongseoki <43495319+dongseoki@users.noreply.github.com>
2019-07-08 16:56:54 +0900

    나무 탈출 실패

---
+++ dongseoki/Dijkstra Algorithm_Bellman Ford/Dijkstra Algorithm_Bellman Ford/tree_escape_15900.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//vector<vector<int> > arr(6, vector<int>(5, 0));
+vector<vector<int> > trvec(500000);            // index 0 사용.
+vector<bool > visited(500000, false);
+
+
+int N;
+int result;
+
+void dfs(int state, int sum) {
+	//visited[state] = true;
+
+	if (trvec[state].empty() == true) {
+		result += sum;
+		return;
+	}
+	for (int i=0; i < trvec[state].size();i++) {
+		if (visited[trvec[state][i]] == false) {
+			visited[trvec[state][i]] = true;
+			visited[state] = true;
+			dfs(trvec[state][i], sum + 1);
+			visited[state] = false;
+			visited[trvec[state][i]] = false;
+			//visited[i] = false; 필요없다.
+		}
+		
+	}
+
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N;
+	int a;
+	int b;
+	for (int i = 0; i < N - 1; i++) {
+		cin >> a;
+		cin >> b;
+		trvec[a - 1].push_back(b - 1);
+		trvec[b - 1].push_back(a - 1);
+	}
+
+	dfs(0, 0);
+	if (result % 2 == 0)
+		cout << "NO";
+	else
+		cout << "YES";
+
+	return 0;
+}
