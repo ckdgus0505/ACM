59162f9f5973a4bd0bbb2ac45f3a4732d0b1adb0
Dong Seok Lee <start0is5half@gmail.com>
2019-08-19 20:50:27 +0900

    1672 failed

--- dongseoki/Algorithm Trouble Solving Strategy/Algorithm Trouble Solving Strategy/2188.cpp
+++ dongseoki/Algorithm Trouble Solving Strategy/Algorithm Trouble Solving Strategy/2188.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int n, m;
 int visited[1010];
 int b[1010];
-//int linknum[51];
 vector<vector<int>> node;
 int dfs(int here) {
 	//cout << "dfs 시작 here값은" << here << "\n";
@@ -16,17 +15,10 @@ int dfs(int here) {
 		return 0;
 	}
 	visited[here] = 1;
-
-	int linknum = 0;
 	for (int i = 0; i < node[here].size(); i++) {
-		
-		/*if (linknum >= 2 && flag==0)
-			return 0;*/ 
-		// 링크는 2개 이상은 안된다.
 		int there = node[here][i];
 		if (!b[there] || dfs(b[there])) {
 			b[there] = here;
-		//	linknum++;
 			return 1;
 		}
 	}
@@ -38,9 +30,7 @@ int bmatch() {
 	for (int i = 1; i <= n; i++) {
 		//cout << i << "번째 bmatch\n";
 		memset(visited, 0, sizeof(visited));
-		dfs(i);
-		memset(visited, 0, sizeof(visited));
-		dfs(i);
+		if (dfs(i))ret++;
 		//cout << "\n 현재 b배열상황\n";
 		//for (int i = 1; i <= n; i++)
 		//	cout << b[i] << " ";
@@ -50,45 +40,20 @@ int bmatch() {
 }
 int main()
 {
-	scanf("%d", &n);
+	scanf("%d %d", &n, &m);
 	node.resize(n + 1);
-	vector<vector<int>> sharks;
-	sharks.push_back({ 0,0,0 });// index 0는 안씀.
+	
 	for (int i = 1; i <= n; i++) {
-		int si, sp, in;
-		scanf("%d %d %d", &si, &sp, &in);
-		sharks.push_back({ si, sp, in });
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == j)
-				continue;
-			int si, sp, in;
-			int count = 0;
-			int eq = 0;
-			for (int k = 0; k < 3; k++) {
-				if (sharks[i][k] < sharks[j][k])
-					count++;
-				else if (sharks[i][k] == sharks[j][k])
-					eq++;
-			}
-			//
-			if (count == 3 || (eq == 3 && i < j)) {
-				//선을 이어주자
-				node[i].push_back(j);
-			}
+		 int Si;
+		scanf("%d", &Si);
+		
+		while (Si--) {
+			int x;
+			scanf("%d", &x);
+			node[i].push_back(x);
 		}
 	}
-	bmatch();
-	//printf("%d\n", bmatch());
-
-	int sum = 0;
-	for (int i = 1; i <= n; i++) {
-		if (b[i]==0)
-			sum++;
-		printf("%d %d \n",i, b[i]);
-	}
-	printf("%d", sum);
+	printf("%d", bmatch());
 	return 0;
 }
 
