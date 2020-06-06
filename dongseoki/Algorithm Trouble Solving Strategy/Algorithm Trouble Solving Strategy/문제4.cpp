df910e1eed98dda609a6b2c44bd7d1d6748c6bfc
dongseoki <43495319+dongseoki@users.noreply.github.com>
2020-06-06 22:35:49 +0900

    변경사항

--- dongseoki/Algorithm Trouble Solving Strategy/Algorithm Trouble Solving Strategy/문제4.cpp
+++ dongseoki/Algorithm Trouble Solving Strategy/Algorithm Trouble Solving Strategy/문제4.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <string.h>
+#include <stdio.h>
 
 #define F(p,q,r) for(int p =q; p<r;p++)
 using namespace std;
@@ -37,6 +39,31 @@ int solution(vector<vector<int>> board) {
 }
 
 int main(void) {
-    vector<vector<int>> board = { {0, 0, 0}, { 0,0,0 }, { 0,0,0 } };
-    solution(board);
+    //vector<vector<int>> board = { {0, 0, 0}, { 0,0,0 }, { 0,0,0 } };
+    //solution(board);
+    char s1[200];
+    char s2[20];
+    char s3[20];
+    char s4[20];
+    int a = 0, 
+        b = 0;
+    //scanf("%s,%s", s1, s1);
+    //printf("%s, %s", s1, s2);
+
+    //scanf("%d:%d", &a, &b);
+    //printf("%d, %d", a, b);
+    scanf("%s", s1);
+    char* ptr = strtok(s1, ":");
+    char* bptr = ptr;
+    printf("출력 시작 \n");
+    int i = 0;
+    while (ptr != NULL)               // 자른 문자열이 나오지 않을 때까지 반복
+    {
+        printf("%d : %s\n",i++, ptr);          // 자른 문자열 출력
+        ptr = strtok(NULL, ":");      // 다음 문자열을 잘라서 포인터를 반환
+        if (bptr == ptr) {
+            printf("그 시기\n");
+        }
+        bptr = ptr;
+    }
 }
